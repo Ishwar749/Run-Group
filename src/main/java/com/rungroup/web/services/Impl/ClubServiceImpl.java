11aa573d771fc539e842dd421b52615ce1f76717
Ishwar749 <19itish083@ldce.ac.in>
2024-02-18 18:00:58 +0530

    Add Get All Club Request

--- src/main/java/com/rungroup/web/services/Impl/ClubServiceImpl.java
+++ src/main/java/com/rungroup/web/services/Impl/ClubServiceImpl.java
@@ -5,9 +5,12 @@ import com.rungroup.web.models.Club;
 import com.rungroup.web.repository.ClubRepository;
 import com.rungroup.web.services.ClubService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Service
 public class ClubServiceImpl implements ClubService {
     private ClubRepository clubRepository;
 
