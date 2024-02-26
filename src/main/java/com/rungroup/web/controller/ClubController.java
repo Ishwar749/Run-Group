9b052a767b8632ef13c3825d0a79ab5396a2ce5e
Ishwar749 <19itish083@ldce.ac.in>
2024-02-26 16:26:55 +0530

    Add Event Service

--- src/main/java/com/rungroup/web/controller/ClubController.java
+++ src/main/java/com/rungroup/web/controller/ClubController.java
@@ -8,10 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -31,6 +28,13 @@ public class ClubController {
         return "clubs-list";
     }
 
+    @GetMapping("/clubs/{clubId}")
+    public String clubDetail(@PathVariable("clubId") long clubId, Model model){
+        ClubDto clubDto = clubService.findClubById(clubId);
+        model.addAttribute("club",clubDto);
+        return "clubs-detail";
+    }
+
     @GetMapping("/clubs/new")
     public String createClubForm(Model model){
         Club club = new Club();
@@ -46,6 +50,19 @@ public class ClubController {
         }
         clubService.saveClub(clubDto);
         return "redirect:/clubs";
+    }
+
+    @GetMapping("/clubs/{clubId}/delete")
+    public String deleteClub(@PathVariable("clubId") long clubId){
+        clubService.delete(clubId);
+        return "redirect:/clubs";
+    }
+
+    @GetMapping("/clubs/search")
+    public String searchClub(@RequestParam(value = "query") String query, Model model){
+        List<ClubDto> clubs = clubService.searchClubs(query);
+        model.addAttribute("clubs",clubs);
+        return "clubs-list";
     }
 
     @GetMapping("/clubs/{clubId}/edit")
